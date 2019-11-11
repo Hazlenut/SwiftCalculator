9c1b3862623f04c08a6c6f983d9cea379c62fd82
Hazlenut <willie.wung@gmail.com>
2019-11-11 04:52:09 -0500

    bolded characters

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,15 +16,15 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         // Do any additional setup after loading the view.
         var array = [UIButton]()
-        let text: [[String]] = [["AC", "+/-", "%", "/"],[ "7", "8", "9", "x"],[ "4","5","6","-" ],[ "1","2,","3","+"]]
+        let text: [[String]] = [["AC", "+/-", "%", "/"],[ "7", "8", "9", "x"],[ "4","5","6","-" ],[ "1","2","3","+"]]
         for i in 0...3 {
             for j in 0...3 {
                 let button: UIButton = UIButton(frame: CGRect(x: CGFloat(i)*CGFloat(UIScreen.main.bounds.width/4), y:CGFloat(UIScreen.main.bounds.height/5) +  CGFloat(j)*CGFloat(UIScreen.main.bounds.height/5), width: CGFloat(UIScreen.main.bounds.width/4), height: CGFloat(UIScreen.main.bounds.height/5)))
                 button.setTitle(text[j][i], for: .normal)
                 button.setTitleColor(.black, for: .normal)
-                button.layer.cornerRadius = 5
                 button.layer.borderWidth = 1
                 button.layer.borderColor = UIColor.black.cgColor
+                button.titleLabel?.font = UIFont(name: "Verdana-Bold", size: 30)
                 self.view.addSubview(button)
                
             }
