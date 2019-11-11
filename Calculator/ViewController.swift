987af238aca4f04e66831f63ccb5af8a4dffd26e
Hazlenut <willie.wung@gmail.com>
2019-11-11 04:44:10 -0500

    first commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,8 +11,22 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        var array = [UIButton]()
+        let text: [[String]] = [["AC", "+/-", "%", "/"],[ "7", "8", "9", "x"],[ "4","5","6","-" ],[ "1","2,","3","+"]]
+        for i in 0...3 {
+            for j in 0...3 {
+                let button: UIButton = UIButton(frame: CGRect(x: CGFloat(i)*CGFloat(UIScreen.main.bounds.width/4), y:CGFloat(UIScreen.main.bounds.height/5) +  CGFloat(j)*CGFloat(UIScreen.main.bounds.height/5), width: CGFloat(UIScreen.main.bounds.width/4), height: CGFloat(UIScreen.main.bounds.height/4)))
+                button.setTitle(text[j][i], for: .normal)
+                button.setTitleColor(.black, for: .normal)
+                self.view.addSubview(button)
+               
+            }
+        }
+            
     }
 
 
